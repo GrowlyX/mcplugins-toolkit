3d405dd80b6175bc9ea3479a4622d5e4e0bfabbf
Subham <growlyxdev@gmail.com>
2023-06-01 08:35:50 -0400

    fix: mark @Export as a qualifier, not a contract

--- shared/src/main/kotlin/io/liftgate/mcplugins/toolkit/export/Export.kt
+++ shared/src/main/kotlin/io/liftgate/mcplugins/toolkit/export/Export.kt
@@ -1,12 +1,14 @@
 package io.liftgate.mcplugins.toolkit.export
 
 import org.jvnet.hk2.annotations.Contract
+import jakarta.inject.Qualifier;
 
 /**
- * Exposed a plugin-local service to other plugins.
+ * Exposed a plugin-local service to other plugins. Exported services
+ * also stay local, and are also used as a service locally.
  *
  * @author GrowlyX
  * @since 5/31/2023
  */
-@Contract
+@Qualifier
 annotation class Export
