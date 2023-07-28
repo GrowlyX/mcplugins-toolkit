38240f99c5e6547c29d826766c3ddacf113882f8
GrowlyX <growlyxdev@gmail.com>
2023-07-28 14:37:20 -0400

    fix: mark SpigotLocalizerFeature as a service

--- spigot/api/src/main/kotlin/io/liftgate/mcplugins/toolkit/spigot/localizer/SpigotLocalizerFeature.kt
+++ spigot/api/src/main/kotlin/io/liftgate/mcplugins/toolkit/spigot/localizer/SpigotLocalizerFeature.kt
@@ -8,11 +8,13 @@ import io.liftgate.mcplugins.toolkit.kompat.getAllServices
 import io.liftgate.mcplugins.toolkit.localizer.LocalizationTemplate
 import org.bukkit.World
 import org.bukkit.entity.Player
+import org.jvnet.hk2.annotations.Service
 
 /**
  * @author GrowlyX
  * @since 7/26/2023
  */
+@Service
 class SpigotLocalizerFeature : CorePluginFeature
 {
     override fun preEnable(plugin: ToolkitPluginContainer)
@@ -31,6 +33,7 @@ class SpigotLocalizerFeature : CorePluginFeature
         plugin.locator
             .getAllServices<LocalizationTemplate>()
             .forEach {
+                // TODO: resource buckets are created with the toolkit's plugin files
                 Localizer.of(it.javaClass.kotlin)
             }
     }
