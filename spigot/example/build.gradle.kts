ce52bf41fe387a7008abea88e449b9c747119b71
GrowlyX <growlyxdev@gmail.com>
2023-07-29 22:49:41 -0400

    feat: add example usage of the RedisFactory

--- spigot/example/build.gradle.kts
+++ spigot/example/build.gradle.kts
@@ -2,5 +2,6 @@ dependencies {
     kapt(libs.platform.spigot.annotate)
     compileOnly(libs.platform.spigot.annotate)
     compileOnly(libs.platform.spigot.api)
+    compileOnly(libs.aware.ktx)
     compileOnly(project(":spigot:spigot-platform"))
 }
---
+++ spigot/example/src/main/kotlin/io/liftgate/mcplugins/toolkit/platform/spigot/example/listener/StaffLoginListener.kt
@@ -0,0 +1,54 @@
+package io.liftgate.mcplugins.toolkit.platform.spigot.example.listener
+
+import io.liftgate.mcplugins.toolkit.contracts.Eager
+import io.liftgate.mcplugins.toolkit.datastore.redis.RedisDatastore
+import io.liftgate.mcplugins.toolkit.profile.StoredPlayerProfileManager
+import io.liftgate.mcplugins.toolkit.spigot.listeners.CoroutineListener
+import jakarta.inject.Inject
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerJoinEvent
+import org.glassfish.hk2.api.PostConstruct
+import org.jvnet.hk2.annotations.Service
+import java.util.UUID
+
+/**
+ * @author GrowlyX
+ * @since 7/29/2023
+ */
+@Service
+class StaffLoginListener : CoroutineListener, Eager, PostConstruct
+{
+    @Inject
+    lateinit var redis: RedisDatastore
+
+    private val connection by lazy {
+        redis.client()
+            .buildNewMessageConnection("staff")
+    }
+
+    @Inject
+    lateinit var profileManager: StoredPlayerProfileManager
+
+    override fun postConstruct()
+    {
+        connection.connect()
+        connection.listen("login") {
+            val profile = profileManager
+                .loadProfileFromUniqueId(
+                    get<UUID>("player")
+                )
+
+            Bukkit.broadcastMessage(
+                "${ChatColor.AQUA}[Staff] ${ChatColor.GREEN}${profile.username}${ChatColor.WHITE} joined!"
+            )
+        }
+    }
+
+    @EventHandler
+    suspend fun PlayerJoinEvent.on()
+    {
+        // TODO:  
+    }
+}
